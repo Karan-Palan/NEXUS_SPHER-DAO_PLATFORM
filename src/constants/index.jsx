cb089913061facaaf532cf27766b7af1a7798ed5
KaranPalan <143683619+Karan-Palan@users.noreply.github.com>
2024-11-15 15:59:57 +0000

    Update profile pic

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -16,21 +16,21 @@ export const testimonials = [
   {
     user: "Karan Palan",
     company: "CTO and co-founder",
-    image: "https://media.licdn.com/dms/image/D5603AQH1ct3NgdBq1A/profile-displayphoto-shrink_200_200/0/1712770824512?e=1718236800&v=beta&t=R15Ww1sTCQLORPICjbxr_qKtUX6Md_uJEGcU42iJ3tQ", // Use a placeholder image URL
+    image: "https://media.licdn.com/dms/image/v2/D5603AQFd4DwGD9a1Ig/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1718313217699?e=1736985600&v=beta&t=Xn20_iUMXwMFg8wLWaa7ZIuVgpKMwXTS-ELHyN3--Q4", // Use a placeholder image URL
     linkedin: "https://www.linkedin.com/in/karan-palan-476472289/", 
     text: "Architecting tomorrow's technology, today.",
   },
   {
     user: "Krish Gupta",
     company: "CEO and co-founder",
-    image: "https://media.licdn.com/dms/image/D5603AQG9vT7EMYm2wg/profile-displayphoto-shrink_200_200/0/1706681242772?e=1720051200&v=beta&t=SM8_uTLlC7ma6F5xAT5vN2kfIhl-spUMmbwSqJnb6Kk", // Use a placeholder image URL
+    image: "https://media.licdn.com/dms/image/v2/D5603AQG9vT7EMYm2wg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1706681242772?e=1736985600&v=beta&t=rmcOXXu5cUXKMfwUA-eqAiEhPej0rGA8HLGj4uz-fpw", // Use a placeholder image URL
     linkedin: "https://www.linkedin.com/in/krish-gupta-51637b1b8/", 
     text: "Driving vision into reality, one strategy at a time.",
   },
   {
     user: "Shravani Wagh",
     company: "COO and co-founder",
-    image: "https://media.licdn.com/dms/image/D5603AQGRGEUKTCB1hg/profile-displayphoto-shrink_200_200/0/1713203509651?e=1718841600&v=beta&t=EMtgpkJtMGNnjuJX-077uH_KdERF66pPjr4fsj9L0Gw", 
+    image: "https://media.licdn.com/dms/image/v2/D5603AQF8-Wm4sfEJeA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1715997732752?e=1736985600&v=beta&t=3qkPL4PzjIh3qlU6SIpUCmXkRtpYYNLxnxYXxM5958E", 
     linkedin: "https://www.linkedin.com/in/shravani-wagh-20a69728a/", 
     text: "Turning chaos into order, with uncompromising precision.",
   },
